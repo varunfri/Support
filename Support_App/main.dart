cfa00d0581b803b4247eba367f1b13fea7d44ed9
Varun's One <68818151+varunfri@users.noreply.github.com>
2023-12-07 19:45:48 +0530

    The Quotes App
    Created a sample app using custom widgets, and card widget.

---
+++ Support_App/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'quotes.dart';
+import 'card.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Awesome_Quotes",
+      home: _HomeThing(),
+    ));
+
+class _HomeThing extends StatelessWidget {
+  final List<Quotes> quotes = [
+    Quotes(text: "Do your things", author: "Varun"),
+    Quotes(text: "Be yourself", author: "Krishna"),
+    Quotes(text: "You are a unique snowflake", author: "Mike")
+  ];
+
+  // List<String> quotes = ["Be your self", "Be Kind"];
+
+  Widget cardTemplate(quote) {
+    return QuoteCard(quote: quote);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: quotes.map((quote) => cardTemplate(quote)).toList(),
+        // children: quotes.map((quotes) {
+        //   return Text(quotes);
+        // }).toList(),
+      ),
+    );
+  }
+}
